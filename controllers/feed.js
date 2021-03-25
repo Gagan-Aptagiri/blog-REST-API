16affaed91008240c0829286bbbb969cb6148d31
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-25 22:07:42 +0530

    Initial controller and test done

---
+++ controllers/feed.js
@@ -0,0 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
+
+exports.getPosts = (req, res, next) => {
+	res.status(200).json({
+		posts: [{ title: 'First Post', content: 'This is the first post!' }],
+	});
+};
+
+exports.createPost = (req, res, next) => {
+	const title = req.body.title;
+	const content = req.body.content;
+	//Create post in db
+	res.status(201).json({
+		message: 'Post has been successfully saved.',
+		post: { id: uuidv4(), title: title, content: content },
+	});
+};
