f77afbe525ae7e3176d43b383a0ab9f42d463c87
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-28 17:38:05 +0530

    JWT signature added for user authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -26,6 +27,47 @@ exports.signUp = (req, res, next) => {
 		})
 		.then((result) => {
 			res.status(201).json({ message: 'User created!', userId: result._id });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.login = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	let loadedUser;
+	User.findOne({ email: email })
+		.then((user) => {
+			if (!user) {
+				const error = new Error('A user with this email could not be found.');
+				error.statusCode = 401;
+				throw error;
+			}
+			loadedUser = user;
+			return bcrypt.compare(password, user.password);
+		})
+		.then((isEqual) => {
+			if (!isEqual) {
+				const error = new Error('Wrong password!');
+				error.statusCode = 401;
+				throw error;
+			}
+			//Creating a token
+			const token = jwt.sign(
+				{
+					email: loadedUser.email,
+					userId: loadedUser._id.toString(),
+				},
+				'secret',
+				{
+					expiresIn: '1h',
+				},
+			);
+			res.status(200).json({ token: token, userId: loadedUser._id.toString() });
 		})
 		.catch((err) => {
 			if (!err.statusCode) {
