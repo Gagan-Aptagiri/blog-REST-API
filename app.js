f4d1eb2286906ee177d999a62814d99dfb7a5a16
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-28 12:24:08 +0530

    Added image functionality

--- app.js
+++ app.js
@@ -1,11 +1,74 @@
+const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, uuidv4() + file.originalname);
+	},
+});
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 app.use(express.json());
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE',
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Content-Type',
+		'Authorization',
+	);
+	next();
+});
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode;
+	const message = error.message;
+	res.status(status).json({ message: message });
+});
+
+mongoose
+	.connect(
+		'mongodb+srv://gags:gags123@alpha.tlchy.mongodb.net/restApi?retryWrites=true&w=majority',
+		{
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		},
+	)
+	.then((result) => {
+		console.log('Connected to the Database.');
+		app.listen(8080, (result) => {
+			console.log('Listening for requests on port 8080');
+		});
+	})
+	.catch((err) => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,17 +1,70 @@
 const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-	res.status(200).json({
-		posts: [{ title: 'First Post', content: 'This is the first post!' }],
+	Post.find().then((posts) => {
+		res
+			.status(200)
+			.json({ message: 'Fetched posts successfully.', posts: posts });
 	});
 };
 
 exports.createPost = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed, entered data is incorrect.');
+		error.statusCode = 422;
+		throw error;
+	}
+	if (!req.file) {
+		const error = new Error('No image provided.');
+		error.statusCode = 422;
+		throw error;
+	}
+
 	const title = req.body.title;
+	const imageUrl = req.file.path;
 	const content = req.body.content;
-	//Create post in db
-	res.status(201).json({
-		message: 'Post has been successfully saved.',
-		post: { id: uuidv4(), title: title, content: content },
+
+	const post = new Post({
+		title: title,
+		imageUrl: imageUrl,
+		content: content,
+		creator: { name: 'Gagan' },
 	});
+	post
+		.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Post has been successfully saved.',
+				post: result,
+			});
+		})
+		.catch((err) => {
+			if (!errors.statusCode) {
+				errors.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.getPost = (req, res, next) => {
+	const postId = req.params.postId;
+	Post.findById(postid)
+		.then((post) => {
+			if (!post) {
+				const error = new Error('Could Not find any post.');
+				error.statusCode = 404;
+				throw error;
+			}
+			res.status(200).json({ message: 'Post fetched.' });
+		})
+		.catch((err) => {
+			if (!errors.statusCode) {
+				errors.statusCode = 500;
+			}
+			next(err);
+		});
 };
