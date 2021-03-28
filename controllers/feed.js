92f229d1a8969a854e5ac963acc041090e558e39
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-28 12:47:07 +0530

    Added update functionality for the posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
+const path = require('path');
+const fs = require('fs');
 
 const Post = require('../models/post');
 
@@ -67,4 +69,54 @@ exports.getPost = (req, res, next) => {
 			}
 			next(err);
 		});
+};
+
+exports.updatePost = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed, entered data is incorrect.');
+		error.statusCode = 422;
+		throw error;
+	}
+	const postId = req.params.postId;
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.imageUrl;
+	if (req.file) {
+		imageUrl = req.file.path;
+	}
+	if (!imageUrl) {
+		const error = new Error('No file picked.');
+		error.statusCode = 422;
+		throw error;
+	}
+	Post.findById(postId)
+		.then((post) => {
+			if (!post) {
+				const error = new Error('Could Not find any post.');
+				error.statusCode = 404;
+				throw error;
+			}
+			if (imageUrl !== post.imageUrl) {
+				clearImage(post.imageUrl);
+			}
+			post.title = title;
+			post.imageUrl = imageUrl;
+			post.content = content;
+			return post.save();
+		})
+		.then((result) => {
+			res.status(200).json({ message: 'Post updated.', post: result });
+		})
+		.catch((err) => {
+			if (!errors.statusCode) {
+				errors.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+const clearImage = (filepath) => {
+	filepath = path.join(__dirname, '..', filePath);
+	fs.unlink(filePath, (err) => console.log(err));
 };
