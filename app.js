ff5fd5ef1028ae40b3c4e27974f6e0065990911e
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-03-28 13:48:52 +0530

    Added user signup funcitionality

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ app.use((error, req, res, next) => {
 	console.log(error);
 	const status = error.statusCode;
 	const message = error.message;
-	res.status(status).json({ message: message });
+	const data = error.data;
+	res.status(status).json({ message: message, data: data });
 });
 
 mongoose
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const { body } = require('express-validator');
+
+const User = require('../models/user');
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/signup');
+router.put(
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.normalizeEmail()
+			.withMessage('Please enter a valid email.')
+			.custom((value, { req }) => {
+				return User.findOne({ email: value }).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject('E-mail address already exists!');
+					}
+				});
+			}),
+		body('password').trim().isLength({ min: 5 }),
+		body('name').trim().not().isEmpty(),
+	],
+	authController.signUp,
+);
 
 module.exports = router;
